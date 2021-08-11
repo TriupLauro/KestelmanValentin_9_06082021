eb999b93f2c97442045932d9321eb0f48c0808f8
Valentin Kestelman <valaourok@gmail.com>
2021-08-11 10:52:09 +0200

    Coverage for BillsUi

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,8 +2,22 @@ import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
+import Bills from "../containers/Bills.js";
 
 describe("Given I am connected as an employee", () => {
+  describe('When the bills page is loading', ()=> {
+    test('Then the loading page should be rendered', () => {
+      document.body.innerHTML = BillsUI({loading: true})
+      expect(screen.getAllByText('Loading...')).toBeTruthy()
+    })
+  })
+  describe('When I am the bills page but there is an error', ()=> {
+    test('Then an error should be displayed', () => {
+      document.body.innerHTML = BillsUI({error: 'some error message'})
+      expect(screen.getAllByText('Erreur')).toBeTruthy()
+    })
+  })
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
       //We need to specify the type of user in order for the vertical layout to load correctly
@@ -22,6 +36,29 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+    })
+    test("Then bill information should be displayed", () =>{
+      const testBill = bills[0]
+      document.body.innerHTML = BillsUI({data: bills})
+      expect(screen.getByText(testBill.name)).toBeTruthy()
+      expect(screen.getByText(testBill.date)).toBeTruthy()
+      expect(screen.getByText(testBill.type)).toBeTruthy()
+      expect(screen.getByText(`${testBill.amount.toString(10)} €`)).toBeTruthy()
+      expect(screen.getByText(testBill.status)).toBeTruthy()
+    })
+    test("Then by clicking on the blue eye icon, a modal should open",() =>{
+      document.body.innerHTML = BillsUI({data: bills})
+      const blueEyeIcon = screen.getAllByTestId('icon-eye')[0]
+      expect(blueEyeIcon).toBeTruthy()
+
+      const bill = new Bills({document})
+      const handleClickIconEye = jest.fn(e => bill.handleClickIconEye(blueEyeIcon))
+      expect(blueEyeIcon).toHaveAttribute('data-bill-url')
+      blueEyeIcon.addEventListener('click',handleClickIconEye)
+
+      userEvent.click(blueEyeIcon)
+      expect(handleClickIconEye).toHaveBeenCalled()
+
     })
   })
 })
